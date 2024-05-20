dcf62ecda5709ef3825692cc09aaeb87987438d8
Marisa258 <167044704+Marisa258@users.noreply.github.com>
2024-05-20 00:58:36 -0400

    Update ClassWorker.py

--- ClassWorker.py
+++ ClassWorker.py
@@ -14,8 +14,14 @@ class Worker:
         return 0
 
     def calculateSatisf(self,actulSal):
-        self.satisf += 2.5 * math.log(1.5 * actualSal / self.exptSal)
-        if (self.satisf > 100) :
+        #calculate using $f\left(x\right)=7\arctan\left(\log_{3}1.2x\right)$; x=actualSal/self.exptSal 
+        #f(0.83)=0, f(1)=1.15, f(2)=4.71, f(3)=6.03, f(0.5)=-3.05, f(0.33)=-4.87, f(0)=-3.5pi
+        if (actualSal / self.exptSal > 0) :
+            self.satisf += 7 * math.atan(math.log(1.2 * actualSal / self.exptSal , a)) 
+        if (actualSal / self.exptSal = 0) :
+            self.satisf -= -3.5 * math.pi
+        
+        if (self.satisf > 100) : 
             self.satisf = 100
         if (self.satisf < 0) :
             self.satisf = 0
