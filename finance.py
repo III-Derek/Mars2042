97a131c10ac9e3e1407316f6f1d4e3d6c1235f76
Derek Cheng <108553294+III-Derek@users.noreply.github.com>
2024-05-22 19:37:46 -0400

    Add files via upload

---
+++ finance.py
@@ -0,0 +1,10 @@
+'''
+finance:
+this finance module allows player to loan at a fixed rate
+based on the player's credit score
+'''
+class finance:
+    '''
+    '''
+    def __init__(self, Player):
+    pass
