8abeaeb45cdda80a5069a7a43a175c9a7fa14e15
Marisa258 <167044704+Marisa258@users.noreply.github.com>
2024-06-03 21:05:41 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import datetime
+externalTime = 
+
+def main():
+    while True:
+        userInput = input() #capital letters only
+
+        if userInput == 'NEW GAME':
+            #...#
+
+            #create new game:#
+            g1=
+            
+
+        if userInput == 'END' :
+            break
+        else:
+            print("NONSENSE");
+
+
+
+if __name__ == "__main__":
+    main()
+
+        
