dfdca5dbcd2f50dca15aa7fefc10a5bf4d42551a
Marisa258 <167044704+Marisa258@users.noreply.github.com>
2024-06-04 21:49:02 -0400

    Update ClassWorker.py

--- ClassWorker.py
+++ ClassWorker.py
@@ -1,7 +1,7 @@
 import math
 import ClassEssentialResources
 
-
+"""
 WorkStats= {
     "name" = ""
     "population" = 0
@@ -11,22 +11,29 @@ WorkStats= {
     "actualProductivity" = 0
     "expectedSalary" = 0
 }
+"""
 
 class Worker:
-    def __init__(self,WorkStats):
-        self.name=name
-        self.population = WorkStats["population"]
-        self.satisf = WorkStats["satisfaction"]
-        self.ifSatisfied=WorkStats["ifSatisfied"]
-        self.prodcty=WorkStats["productivity"]
-        self.actulProdcty=WorkStats["actualProductivity"]
-        self.exptSal=WorkStats["expectedSalary"]
+    def __init__(self,name):
+        """
+        name: str. 
+        fields info see below
+        """
+        self.name=name  
+        self.population = 0 #int>=0
+        self.satisf = 50 #float, between 0 and 100
+        self.ifSatisfied= 1 #bool, becomes 0 if satisf<20
+        self.prodcty= 0 #float
+        self.actulProdcty= 0 #float
+        self.exptSal= 0 #float
 
     def getName():
+        #print name of the worker.
         print(self.name)
     
     def updateExptSal(self):
-        #depends on prices of electricity, water and food linearly
+        #update and return expected salary.
+        #expected salary depends on prices of electricity, water and food linearly.
         k1 = 1 # to be determined
         k2 = 1
         k3 = 1
@@ -36,9 +43,11 @@ class Worker:
     
         return self.exptSal
 
-    def updateSatisf(self,actulSal):
-        #calculate using $f\left(x\right)=7\arctan\left(\log_{3}1.2x\right)$; x=actualSal/self.exptSal 
+    def updateSatisf(self):
+        #update and return satisfaction based on ratio of actual salary and expected salary.
+        #formula: current satisfaction + $f\left(x\right)=7\arctan\left(\log_{3}1.2x\right)$; x=actualSal/self.exptSal 
         #f(0.83)=0, f(1)=1.15, f(2)=4.71, f(3)=6.03, f(0.5)=-3.05, f(0.33)=-4.87, f(0)=-3.5pi
+        
         if (actualSal / self.exptSal > 0) :
             self.satisf += 7 * math.atan(math.log(1.2 * actualSal / self.exptSal , a)) 
         if (actualSal / self.exptSal = 0) :
@@ -49,7 +58,7 @@ class Worker:
         if (self.satisf < 0) :
             self.satisf = 0
 
-        if (self.satisf < 20) or (self.satisf = 20):
+        if (self.satisf < 20) :
             ifSatisfied = 0
         
         return self.satisf 
@@ -59,6 +68,9 @@ class Worker:
         return self.prodcty
 
     def updateActulProdcty(self):
+        #update and return actual productivity.
+        #formula: self.prodcty * (self.satisf / 50) if ifSatisfied=1; else 0.
+        
         if (ifSatisfied = 0) :
             self.prodcty = 0
             print(WORKERS ON STRIKE!)
